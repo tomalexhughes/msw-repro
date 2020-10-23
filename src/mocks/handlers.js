711fe3ba47c019aca1af016dc19152a4ea80644a
Tom <tom-personal@Toms-MacBook-Pro.local>
2020-10-23 21:03:04 +0100

    Adds further logging to reproduction

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,8 +2,10 @@ import { rest } from 'msw';
 
 export const handlers = [
   rest.post('/example', (req, res, ctx) => {
-    //typeof returns 'string' in browser and 'object' in tests
-    console.log({ req }, typeof req.body);
+    const isJson = req.headers.get('content-type')?.endsWith('json')
+    const isValidJson = isJson && req.body?.constructor.name === 'Object'
+    const type = typeof req.body // typeof returns 'string' in browser and 'object' in tests
+    console.log({ req, isJson, isValidJson, type });
     return res(ctx.json(true));
   })
 ];
